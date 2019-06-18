1763c0e73549d3f7e0125aac5c6b6633957ca693
TheOnlySmartBoy <o2jose43@gmail.com>
2019-06-18 18:48:24 +0300

    Begun the login logic

--- auth/login.php
+++ auth/login.php
@@ -2,3 +2,15 @@
 /*
 *Author TheOnlySmartBoy<o2jose43@gmail.com>
 */
+require '../db/connector.php';
+$uname = "";
+$email = "test@test.com";
+$pass = "MasterTosby2";
+$query ="SELECT * FROM tbl_users WHERE email= '$email' AND password = '$pass'";
+$result = mysqli_query($conn, $query);
+if(mysqli_num_rows($result)>0){
+    echo 'Login Successfull';
+}
+else{
+    echo 'Login Failed';
+}
