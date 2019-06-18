eef353becae98fa63bc2783dbe9d3a83c467eb78
TheOnlySmartBoy <o2jose43@gmail.com>
2019-06-18 18:30:09 +0300

    Completed database connection logic

--- db/connector.php
+++ db/connector.php
@@ -3,5 +3,14 @@
 /*
 *Author TheOnlySmartBoy<o2jose43@gmail.com>
 */
+$database ="foodfuz_db";
+$user ="Tosby";
+$password ="MasterTosby2";
+$port = "3309";
+$host = "localhost";
+$conn = mysqli_connect($host, $user, $password, $database, $port);
+if(!$conn){
+    echo mysqli_error($conn);
+}
 
 
