4ac1a907ec2ddd52f5718c9c99817c0db09b3cb5
TheOnlySmartBoy <o2jose43@gmail.com>
2019-06-20 20:16:39 +0300

    Housekeeping

--- auth/register.php
+++ auth/register.php
@@ -1,22 +1,21 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] =='POST'){
-require_once '../db/connector.php';
-    $name = mysqli_real_escape_string($conn, $_POST['name']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require_once '../db/connector.php';
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
     $pass = password_hash($password, PASSWORD_DEFAULT);
-   
-    $sql = "INSERT INTO users_table (name, email, password) VALUES ('$name', '$email', '$pass')";
 
-    if ( mysqli_query($conn, $sql) ) {
+    $sql = "INSERT INTO tbl_users (name, email, password) VALUES ('$name', '$email', '$pass')";
+
+    if (mysqli_query($conn, $sql)) {
         $result["success"] = "1";
         $result["message"] = "success";
 
         echo json_encode($result);
         mysqli_close($conn);
-
     } else {
 
         $result["success"] = "0";
