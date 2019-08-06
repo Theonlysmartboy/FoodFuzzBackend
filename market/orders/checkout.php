29cb398e0567aa4919ce1c0ec1e67a02c8ea1b55
TheOnlySmartBoy <o2jose43@gmail.com>
2019-08-06 18:37:50 +0300

    Added frontend placeholder images

--- market/orders/checkout.php
+++ market/orders/checkout.php
@@ -1,27 +1,27 @@
 <?php
-require_once '../../db/connector.php';
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $sql = "SELECT * FROM view_products WHERE category= 'drink' ";
-    $response = mysqli_query($conn, $sql);
-    $result = array();
-    $result['drink'] = array();
-    if (mysqli_num_rows($response) > 0) {
-         while($row = mysqli_fetch_assoc($response)){
-        $index['name'] = $row['name'];
-        $index['description'] = $row['descr'];
-        $index['image'] = $row['image'];
-        $index['cost'] = $row['cost'];
-        $index['seller'] = $row['restaurant'];
-        $index['id'] = $row['id'];
-        array_push($result['drink'], $index);
-        $result['success'] = "1";
-        $result['message'] = "success";
-         }
+    require_once '../db/connector.php';
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $tel = mysqli_real_escape_string($conn, $_POST['tel']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    $pass = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO tbl_orders (name, email, tel, password) VALUES ('$name', '$email', '$tel', '$pass')";
+
+    if (mysqli_query($conn, $sql)) {
+        $result["success"] = "1";
+        $result["message"] = "success";
+
         echo json_encode($result);
         mysqli_close($conn);
     } else {
-        $result['success'] = "0";
-        $result['message'] = "error";
+
+        $result["success"] = "0";
+        $result["message"] = "error";
+
         echo json_encode($result);
         mysqli_close($conn);
     }
