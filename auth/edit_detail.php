b7687adba14248094bf3ece94bb1700f8aef2a9f
TheOnlySmartBoy <o2jose43@gmail.com>
2019-12-02 13:02:50 +0300

    Finished writing the update user data code

--- auth/edit_detail.php
+++ auth/edit_detail.php
@@ -1,28 +1,27 @@
 <?php
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
- require_once '../db/connector.php';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require_once '../db/connector.php';
+    $id = mysqli_real_escape_string($conn, $_POST['Id']);
     $name = mysqli_real_escape_string($conn, $_POST['name']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $id = mysqli_real_escape_string($conn, $_POST['id']);
+    $phone = mysqli_real_escape_string($conn, $_POST['phone']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $pass = password_hash($password, PASSWORD_DEFAULT);
 
-       $sql = "UPDATE users_table SET name='$name', email='$email' WHERE id='$id' ";
+    $sql = "UPDATE tbl_users SET name='$name', email='$email', tel='$phone', password='$pass' WHERE id='$id' ";
 
-    if(mysqli_query($conn, $sql)) {
+    if (mysqli_query($conn, $sql)) {
+        $response["error"] = FALSE;
+        $response["message"] = "success";
+        echo json_encode($response);
+        mysqli_close($conn);
+    }
+} else {
+    // user is not found with the credentials
+        $response["error"] = TRUE;
+        $response["error_msg"] = "Unable to update data. Please try again!";
+        echo json_encode($response);
 
-          $result["success"] = "1";
-          $result["message"] = "success";
-
-          echo json_encode($result);
-          mysqli_close($conn);
-      }
-  }
-
-else{
-
-   $result["success"] = "0";
-   $result["message"] = "error!";
-   echo json_encode($result);
-
-   mysqli_close($conn);
+    mysqli_close($conn);
 }
