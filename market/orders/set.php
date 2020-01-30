d1b9440d7726a1ec008a0dfb8bfd37998515636d
TheOnlySmartBoy <o2jose43@gmail.com>
2020-01-30 15:34:23 +0300

    Begun writing the set payment logic

---
+++ market/orders/set.php
@@ -0,0 +1,30 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require_once '../../db/connector.php';
+    $orderId = mysqli_real_escape_string($conn, $_POST['orderId']);
+    $delivery_fee = mysqli_real_escape_string($conn, $_POST['delivery']);
+    $totalAmt = mysqli_real_escape_string($conn, $_POST['total']);
+    $status = 0;
+    $sql = "INSERT INTO payments (orderid, delivery_fee, total, status) "
+            . "VALUES ('$orderId', '$', '$name', '$seller', '$amount', "
+            . "'$quantity', '$longitude', '$latitude', '$location', '$status')";
+
+    if (mysqli_query($conn, $sql)) {
+        $result["success"] = "1";
+        $result["message"] = "success";
+        echo json_encode($result);
+        mysqli_close($conn);
+    } else {
+
+        $result["success"] = "0";
+        $result["message"] = "An error occured ".mysqli_error(connection);
+        echo json_encode($result);
+        mysqli_close($conn);
+    }
+} else {
+    $result["success"] = "0";
+    $result["message"] = "error";
+    echo json_encode($result);
+    mysqli_close($conn);
+}
